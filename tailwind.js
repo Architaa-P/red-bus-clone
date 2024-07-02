a18373668b6e08fadba0f7f22b1c0f47e801d712
Architaa-P <architaprakash779@gmail.com>
2024-07-03 01:21:39 +0530

    Add files via upload

---
+++ tailwind.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Mont: ['Montserrat']
+      },
+      colors: {
+        'main-color' : '#D84E55',
+      }
+    },
+  },
+  plugins: [],
+}
